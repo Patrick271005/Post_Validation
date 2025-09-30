794cecaea890cdfe4f8ccad4f28e965349de48aa
Patrick Flanagan <G00431255@atu.ie>
2025-09-30 10:22:54 +0100

    Created Project

---
+++ src/main/java/ie/atu/lab2a/Lab2AApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2AApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab2a/Lab2AApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
